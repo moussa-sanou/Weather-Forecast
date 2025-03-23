1d3caf3f3126a4a1d611f058d949be1f8d560304
samidollars <muss.junior@gmail.com>
2025-03-22 22:31:03 -0500

    Project Weather app Download a add json file reader.

--- WeatherApp.java
+++ WeatherApp.java
@@ -1,10 +1,20 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class WeatherApp {
 
     public static void main(String[] args) throws IOException {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What city would you like to see the weather forecast for?: ");
+        String city = scanner.nextLine();
+
         WeatherAPI api = new WeatherAPI();
-        System.out.println(api.getForecast("Minneapolis"));
+        String forecasting = api.getForecast(city);
+
+        WeatherResponseParser parser = new WeatherResponseParser();
+        parser.parseAndPrint(forecasting);
+
 
     }
 }
